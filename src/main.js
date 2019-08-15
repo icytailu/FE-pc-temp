e92eab18e2b6d9ba573afdaaf2141b006c088828
icytail <1307881527@qq.com>
2019-08-15 12:12:11 +0800

    组件注册

--- src/main.js
+++ src/main.js
@@ -2,9 +2,26 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 
+import "./assets/stylus/index.styl";
+
+// 引入全局组件注册
+import componentRegister from "./utils/componentsImport";
+
+// 引入第三方组件
+import "./libs/eleComponentRegist";
+
+// 引入全局方法
+import "./utils/addPrototypeMethods";
+
+// 全局Vue过滤器
+import * as filterObj from "./utils/vueFilter";
+Object.keys(filterObj).forEach(key => Vue.filter(key, filterObj[key]));
+
 Vue.config.productionTip = false;
 
 new Vue({
+  componentRegister,
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created() {}
 }).$mount("#app");
